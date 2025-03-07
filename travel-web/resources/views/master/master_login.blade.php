2e018504bf09cd1d621b8b784488096befa843a6
ilham hendro prasetyo <ilhamhendroprasetyo@gmail.com>
2025-03-08 05:21:39 +0700

    template

---
+++ travel-web/resources/views/master/master_login.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>@yield('title')</title>
+    <link href="{{ asset('admin/css/styles.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('admin/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/css/all.css') }}">
+</head>
+
+<body class="bg-primary">
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                @yield('main')
+            </main>
+        </div>
+        <div id="layoutAuthentication_footer">
+            @include('master.login.footer')
+        </div>
+    </div>
+    <script src="{{ asset('admin/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('admin/js/all.js') }}"></script>
+    <script src="{{ asset('admin/js/scripts.js') }}"></script>
+    @yield('js')
+</body>
+
+</html>
